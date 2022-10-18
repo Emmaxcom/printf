a8f559bdf13bd968117bd5a134a41d6604ff0d3b
Emmaxcom <emmaxcom@yahoo.com>
2022-10-19 00:47:44 +0100

    printf task

--- utils.c
+++ utils.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
@@ -8,12 +7,10 @@
  */
 int is_printable(char c)
 {
-	if (c >= 32 && c < 127)
-		return (1);
-
-	return (0);
+if (c >= 32 && c < 127)
+return (1);
+return (0);
 }
-
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
@@ -23,18 +20,18 @@ int is_printable(char c)
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
-	if (ascii_code < 0)
-		ascii_code *= -1;
+char map_to[] = "0123456789ABCDEF";
+/* The hexa format code is always 2 digits long */
+if (ascii_code < 0)
+ascii_code *= -1;
 
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
+buffer[i++] = '\\';
+buffer[i++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+buffer[i++] = map_to[ascii_code / 16];
+buffer[i] = map_to[ascii_code % 16];
 
-	return (3);
+return (3);
 }
 
 /**
@@ -45,10 +42,10 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  */
 int is_digit(char c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
+if (c >= '0' && c <= '9')
+return (1);
 
-	return (0);
+return (0);
 }
 
 /**
@@ -60,12 +57,12 @@ int is_digit(char c)
  */
 long int convert_size_number(long int num, int size)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((short)num);
+if (size == S_LONG)
+return (num);
+else if (size == S_SHORT)
+return ((short)num);
 
-	return ((int)num);
+return ((int)num);
 }
 
 /**
@@ -77,10 +74,10 @@ long int convert_size_number(long int num, int size)
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((unsigned short)num);
+if (size == S_LONG)
+return (num);
+else if (size == S_SHORT)
+return ((unsigned short)num);
 
-	return ((unsigned int)num);
+return ((unsigned int)num);
 }
